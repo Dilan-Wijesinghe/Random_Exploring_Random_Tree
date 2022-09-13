763d301d8c87568c4e6e31abc3c7fdfdce7c2af1
Dilan-Wijesinghe <dilan.wijesinghe0@gmail.com>
2022-09-13 14:38:27 -0500

    Added Bug Fixes
    Added some Bug Fixes to expand() function

--- rrt.py
+++ rrt.py
@@ -3,6 +3,8 @@ from pickletools import uint8
 import random 
 import numpy as np
 from numpy import linalg as LA
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 class Node:
     def __init__(self,p_node=np.zeros(2),c_node=np.zeros(2),pos=np.zeros(2)):
@@ -15,18 +17,18 @@ class RRT:
         self.K = K # Num Vertices
         self.delta = delta # Incremental Dist
         self.D = D # Planning Domain   
-        self.q_init = q_init # Inital Position/Configuration
+        self.q_init = q_init # Inital Position/Configuration (Node type)
 
     def expand(self):
-        G = np.empty()           # G is a list of vertices
+        G = []             # G is a list of vertices
         G.append(self.q_init) # Initialize G with q_init
-        for i in range(0,self.K): # Repeat K times
-            q_rand = self.rand_config() # Generates a rand pos in D
-            q_near = self.nearest_vertex(q_rand, G) # Find closest node
-            q_new = self.new_config(q_near, q_rand)
-            G.append(q_new)
-            # Add a vertex between q_near and q_new (Built-in to new_config)
-        return G
+        # for i in range(0,self.K): # Repeat K times
+        #     q_rand = self.rand_config() # Generates a rand pos in D
+        #     q_near = self.nearest_vertex(q_rand, G) # Find closest node
+        #     q_new = self.new_config(q_near, q_rand)
+        #     G.append(q_new)
+        #     # Add a vertex between q_near and q_new (Built-in to new_config)
+        # return G
 
     def rand_config(self):
         # Generate Node with random position in D
@@ -37,10 +39,11 @@ class RRT:
     def nearest_vertex(self, GivenNode, Graph):
         # Graph is a list of Nodes
         pos = GivenNode.pos # Gets positon as np.array
-        dists = np.empty()
+        dists = []
         for node in Graph:
             euc_dist = LA.norm(pos - node)
             dists.append(euc_dist)
+        dists = np.array(dists)
         
         # print("Index is:", np.argmin(dists))
         # print("Node is:", Graph[np.argmin(dists)].pos) # Prints pos of nearest node
@@ -50,7 +53,7 @@ class RRT:
     """ Will return a Node with the new configuration """
     def new_config(self, NearestNode, RandomNode):
         # self.delta needed here
-        dir = RandomNode - NearestNode # Element-wise subtraction of nodes 
+        dir = RandomNode.pos - NearestNode.pos # Element-wise subtraction of nodes 
         dir = dir / self.mag(dir) # Get Unit Vector
         dir = dir * self.delta # Multiply by Delta
 
@@ -65,7 +68,26 @@ class RRT:
     def mag(self,vector: np.array):
         return np.sqrt(vector.dot(vector))
 
-TestRRT = RRT(10,1,np.zeros((100,100)),Node(pos=np.array([50,50])))
-vec = np.array([4,3])
-print(np.sqrt(vec.dot(vec)))
-print(vec / TestRRT.mag(vec))
+    def grow_tree(self, Graph):
+        points = []
+        for node in Graph:
+            points.append(node.pos)
+        points = np.array(points)
+        plt.scatter(points[0], points[1])
+        plt.show()
+
+
+
+TestRRT = RRT(K=10,delta=1,D=np.array([100,100]),q_init=Node(pos=np.array([50,50])))
+Graph = TestRRT.expand()
+
+# Magnitude Tests
+# vec = np.array([4,3])
+# print(np.sqrt(vec.dot(vec)))
+# print(vec / TestRRT.mag(vec))
+
+# Plotting Test
+# points = (np.random.rand(100), np.random.rand(100))
+# plt.scatter(points[0], points[1])
+# plt.show()
+
